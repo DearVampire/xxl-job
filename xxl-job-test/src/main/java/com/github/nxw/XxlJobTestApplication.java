81bb6ce6926e993315a14561553b9845166f253f
牛晓文 <niuxiaowen@halosee.com>
2021-05-25 19:50:39 +0800

    xxl-job project build

---
+++ xxl-job-test/src/main/java/com/github/nxw/XxlJobTestApplication.java
@@ -0,0 +1,16 @@
+package com.github.nxw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auth：niuxiaowen
+ * @Desc:
+ * @Date: 2021/5/25
+ */
+@SpringBootApplication
+public class XxlJobTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobTestApplication.class);
+    }
+}
